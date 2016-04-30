1d5ea940c18146957065570fedc093b2f8506315
Sekhar Paladugu <sekharp@gmail.com>
2016-04-30 16:27:49 -0600

    table headers for columns and rows rendering properly for combined days

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all.order("start ASC")
+    @tracks = Event.uniq.pluck(:track).sort.select {|t| t[0] == "T"}
+    @starts = Event.uniq.pluck(:start).sort
   end
 end
