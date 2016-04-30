7730e2cdecaae26db6d6fc881f2e5e562cac8b50
Sekhar Paladugu <sekharp@gmail.com>
2016-04-30 17:39:29 -0600

    formatting track and doing database call on model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ActiveRecord::Base
+  def self.format_track(start, track)
+    event ||= Event.find_by(start: start, track: track)
+    all_track_event ||= Event.find_by(start: start, track: ["Lunch", "Keynote", "Coffee Breaks", "Party"])
+    if event
+      event.name
+    elsif all_track_event
+      all_track_event.name
+    end
+  end
 end
