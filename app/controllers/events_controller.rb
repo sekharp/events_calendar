9bc8fcfc1fe54c3fac705713a80ab564436001bd
Sekhar Paladugu <sekharp@gmail.com>
2016-04-30 19:03:31 -0600

    time buttons now leading to mostly filtered views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,11 @@ class EventsController < ApplicationController
   end
 
   def show
-    @events = Event.all.order("start ASC")
+    month = params["id"][0..1]
+    day = params["id"][3..4]
+    real_date = day + "/" + month + "/" + params["id"][6..9]
+    t = Time.parse(real_date)
+    @events = Event.where(start: t..(t+1.day)).order("start ASC")
     @event = Event
     @tracks = Event.uniq.pluck(:track).sort.select { |t| t[0] == "T" }
     @times = @events.uniq.pluck(:start, :end)
