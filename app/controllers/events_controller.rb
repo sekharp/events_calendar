de8c0adae4bae65024e253298abf80092cfcfd35
Sekhar Paladugu <sekharp@gmail.com>
2016-04-30 18:24:20 -0600

    buttons working and going from new index to show page, but days not filtering yet

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,12 @@
 class EventsController < ApplicationController
   def index
+    @dates = Event.uniq.pluck(:start).map! { |t| t.strftime("%m/%d/%Y") }.uniq
+  end
+
+  def show
     @events = Event.all.order("start ASC")
-    @tracks = Event.uniq.pluck(:track).sort.select {|t| t[0] == "T"}
-    @starts = Event.uniq.pluck(:start).sort
+    @event = Event
+    @tracks = Event.uniq.pluck(:track).sort.select { |t| t[0] == "T" }
     @times = @events.uniq.pluck(:start, :end)
   end
 end
